79136a985b82ffd0f66cc6e4583e87f5ae63939e
potato0217 <cgu8804103@gmail.com>
2026-05-17 18:04:47 +0900

    테스트 페이지 커밋

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,7 +3,7 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `potato0217-github-blog`,
-    siteUrl: `https://www.potato0217.github.com`
+    siteUrl: `https://potato0217.github.io/`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
